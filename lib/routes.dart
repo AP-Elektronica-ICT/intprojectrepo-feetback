ae83bcfe94801e3af43bde171a83b7186c2dfe6a
Olivier Verhaegen <olivierverhaegen1@hotmail.com>
2020-02-09 13:43:57 +0100

    Creates basic route

---
+++ lib/routes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+
+import 'package:feetback/screens/homePage/home.dart';
+
+final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
+  "/": (BuildContext context) => MyHomePage(),
+};
