3eb2fc5a3aad8a50a4862a2f9e228b267dac0ede
ben <s108652@ap.be>
2020-02-18 16:04:21 +0100

    made RouteGenerator using MaterialPageRoute

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,50 @@
-import 'package:flutter/widgets.dart';
 
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:feetback/screens/homePage/home.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
   "/": (BuildContext context) => MyHomePage(title: "hello",),
 };
+
+
+class RouteGenerator {
+    static Route<dynamic> generateRoute(RouteSettings settings){
+      //getting arguments passed in while calling Navigator.pushNamed
+      final args = settings.arguments;
+
+      switch(settings.name){
+
+        case '/' : 
+          return MaterialPageRoute(builder: (_) => MyHomePage());
+          break; 
+        case '/second' : 
+          if(args is String){
+            return MaterialPageRoute(builder: (_) => MyHomePage());
+          }
+
+          return _errorRoute();
+          break;
+        default:
+          return _errorRoute();
+        break;
+        
+      }
+    }
+
+    static Route<dynamic> _errorRoute(){
+      return MaterialPageRoute(builder: (_) {
+        return Scaffold(
+        appBar: AppBar (
+          title : Text('error'),
+          ),
+        body: Center(
+          child: Text('error'),
+          ),
+
+        );
+      });
+      
+    }
+}
+
