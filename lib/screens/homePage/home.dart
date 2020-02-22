31caf39017178ff6e0267b1b02f35eefa96a1ccd
Olivier Verhaegen <olivierverhaegen1@hotmail.com>
2020-02-22 21:02:55 +0100

    Centered button

--- lib/screens/homePage/home.dart
+++ lib/screens/homePage/home.dart
@@ -4,10 +4,22 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          child: Text("Jump history"),
-          onPressed: () => Navigator.pushNamed(context, "/jump_history")
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Text("Logged in as: "),
+            RaisedButton(
+              child: Text("Jump history"),
+              onPressed: () => Navigator.pushNamed(context, "/jump_history")
+            ),
+          ],
         ),
       )
     );
