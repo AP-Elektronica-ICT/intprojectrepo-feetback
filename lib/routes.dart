792244af26e1c48d674d43b5d9266d3dafcd4d6e
OlivierVerhaegen <56387556+OlivierVerhaegen@users.noreply.github.com>
2020-02-22 20:22:24 +0100

    Update routes.dart

--- lib/routes.dart
+++ lib/routes.dart
@@ -4,6 +4,5 @@ import 'package:feetback/screens/homePage/home.dart';
 import 'package:feetback/screens/jumpHistory/jump_history.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
-  //"/": (BuildContext context) => MyHomePage(title: "FeetBack",),
-  "/": (BuildContext context) => JumpHistoryPage(),
+  "/jump_history": (BuildContext context) => JumpHistoryPage(),
 };
