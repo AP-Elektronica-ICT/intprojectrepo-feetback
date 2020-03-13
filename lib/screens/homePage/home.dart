8618c55ae8c5e180ca2200c2b3b96e57f0980629
ben <s108652@ap.be>
2020-03-13 20:22:57 +0100

    naviagtion bar added

--- lib/screens/homePage/home.dart
+++ lib/screens/homePage/home.dart
@@ -1,43 +1,84 @@
 import 'package:feetback/models/jump.dart';
+
 import 'package:flutter/material.dart';
 
 import 'package:feetback/widgets/feetback_app_bar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Index 0 : Home'),
+    Text('Index 1 : other'),
+    Text('Index 2 : also other')
+  ];
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+
+      switch (index) {
+        case 0:
+          Navigator.pushNamed(context, "/jumphistory");
+          break;
+        case 1:
+          Navigator.pushNamed(context, "/signin");
+          break;
+        case 2:
+          Navigator.pushNamed(context, "/jumpdetails",
+              arguments: Jump(DateTime.now(), 177, 4));
+          break;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: FeetbackAppBar(
-        title: Text("Home"),
-        height: 92,
-        contentAlignment: Alignment.centerLeft,
-        padding: EdgeInsets.only(left: 16, right: 16),
+        title: const Text("Home"),
+        
       ),
-      body: Container(
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            Text("Logged in as: "),
-            RaisedButton(
-              child: Text("Jump Details"),
-          
-              onPressed: () => Navigator.pushNamed(context, "/jumpdetails" , arguments: Jump(DateTime.now(), 177, 4)), //dummy data to test arguments passing
-              
-            ),
-            RaisedButton(
-              child: Text("SignIn"),
-              onPressed: () => Navigator.pushNamed(context, "/signin"),
-            ),
-            RaisedButton(
-              child: Text("Jump History"),
-              onPressed: () => Navigator.pushNamed(context, "/jumphistory" ), 
-            ),
-          ],
-        ),
-      )
+      body: Padding(
+        padding: EdgeInsets.all(16.4),
+          child: Container(
+            child:  ,),),
+
+
+      bottomNavigationBar: BottomNavigationBar(
+        
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text(''),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.brightness_auto),
+            title: Text(''),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.accessibility_new),
+            title: Text(''),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Theme.of(context).accentColor,
+        backgroundColor: Colors.white,
+        iconSize: 20.0,
+        
+
+      
+        onTap: _onItemTapped,
+        
+      ),
+
+      
+       
     );
   }
 }
