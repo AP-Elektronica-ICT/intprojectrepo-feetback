082393cf798c743ad5d6944fee4c96e181f298dc
ben <s108652@ap.be>
2020-03-20 15:59:00 +0100

    styling done

--- lib/screens/homePage/home.dart
+++ lib/screens/homePage/home.dart
@@ -1,11 +1,9 @@
-
 import 'package:flutter/material.dart';
 
 import 'package:feetback/models/jump.dart';
 import 'package:feetback/screens/detailedJumpPage/jump_detailed.dart';
 import 'package:feetback/services/bluetooth_service.dart';
 import 'package:feetback/widgets/feetback_app_bar.dart';
-
 
 class HomePage extends StatefulWidget {
   static const List<Widget> _widgetOptions = <Widget>[
@@ -40,81 +38,99 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  var jumping = Row(children: <Widget>[Image(image: AssetImage("lib/images/jump-illustration.png"))],);
-
+  var jumping = Row(
+    children: <Widget>[
+      Image(image: AssetImage("lib/images/jump-illustration.png"))
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: FeetbackAppBar(
-        title: const Text("Home"),
-        height: 92,
-        contentAlignment: Alignment.centerLeft,
-        padding: EdgeInsets.only(left: 16, right: 16),
-        
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16),
-        
-        
-        child: Container(
-          child: Center(
+        appBar: FeetbackAppBar(
+          title: const Text("Home"),
+          height: 92,
+          contentAlignment: Alignment.centerLeft,
+          padding: EdgeInsets.only(left: 16, right: 16),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(32),
+          child: Container(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                
-                Image(image: AssetImage("lib/images/jump-illustration.png")),
+                Center(child: Image(image: AssetImage("lib/images/jump-illustration.png"))),
+
+                Row(
+                  children: <Widget>[
+                    Image(image: AssetImage("lib/images/Icon-material-history.png")),
+                    SizedBox(width : 16),
+                    Text('80 cm' , style : Theme.of(context).textTheme.headline4.copyWith( fontWeight: FontWeight.bold)),
+                  ],
+                ),
                 
 
+                
+
+                SizedBox(height : 32),
+
+                Text('Instructions', style : Theme.of(context).textTheme.headline5.copyWith( fontWeight : FontWeight.bold)),
+
+                SizedBox(height : 16),
+
+                Text('1. Stand on the mat, align your feet to the pads', style : Theme.of(context).textTheme.subtitle1),
+
+                SizedBox(height : 32),
+
+                Text('2. When you press JUMP we will start counting down form 3, jump on GO.', style : Theme.of(context).textTheme.subtitle1),
+
+                SizedBox(height : 32),
+
+                Text('3. Try to land with both feet on the pads.', style : Theme.of(context).textTheme.subtitle1),
+
+
+                
               ],
             ),
 
+          ),
         ),
-      ),
-
-      ),
-    
-     
 
 
 
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: (){
 
-        },
-        label: const Text('Jump'),
-        backgroundColor: Colors.red
-        
-        
-      )
-    ,
-        
-        
 
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text(''),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.show_chart),
-            title: Text(''),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            title: Text(''),
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Theme.of(context).accentColor,
-        backgroundColor: Colors.white,
-        iconSize: 24.0,
-        onTap: _onItemTapped,
-      )
 
-    );
-      
-  
-    
+        floatingActionButton: FloatingActionButton.extended(
+            onPressed: () {},
+            label: const Text('JUMP'),
+            backgroundColor: Colors.red),
+            
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('home'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.show_chart),
+              title: Text('chart'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              title: Text('settings'),
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Theme.of(context).accentColor,
+          backgroundColor: Colors.white,
+          elevation: 0 ,
+          iconSize: 24.0,
+          onTap: _onItemTapped,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+
+          
+        ));
   }
 }
