843d6fda4cc01f71eaaab6a6f9b98728d46ce174
Thibaut <weekx.t@gmail.com>
2020-03-25 16:05:24 +0100

    Parse & toString()
    Added a method that parses a Json object to a costum jump-object
    
    Override method of toString() that converts the jump to  a string with indents and new lines

--- lib/models/jump.dart
+++ lib/models/jump.dart
@@ -1,13 +1,38 @@
+import 'package:firebase_database/firebase_database.dart';
+
 class Jump{
-    DateTime date;
-    double height;
-    double airtime;
-    bool favorite;
-  
-    Jump(DateTime date, double height, double airtime){
-        this.date = date;
-        this.height = height;
-        this.airtime = airtime;
-        this.favorite = false;
-    }    
+  String jid;
+  DateTime date;
+  double height;
+  double airtime;
+  bool favorite;
+
+  Jump(DateTime date, double height, double airtime){
+      this.date = date;
+      this.height = height;
+      this.airtime = airtime;
+      this.favorite = false;
+  }
+
+  Jump.fromDB(this.jid, Map data) {
+    date = DateTime.parse(data['date']);
+    height = data['height'];
+    airtime = data['airtime'];
+    favorite = data['favorite'];
+  }
+
+  @override
+  String toString() {
+    //   print(jump.jid);
+    //   print('\t' + jump.height.toString());
+    //   print('\t' + jump.date.toString());
+    //   print('\t' + jump.airtime.toString());
+    //   print('\t' + jump.favorite.toString());
+    //   print('\n\n');
+    return "jid: " + jid 
+          + '\n\t' + "date:     " + date.toString()
+          + '\n\t' + "height:   " + height.toString()
+          + '\n\t' + "airtime:  " + airtime.toString()
+          + '\n\t' + "favorite: " + favorite.toString();
+  }
 }
