ff1cf04bb149eb902642306fb3aeeab168853630
Thibaut <weekx.t@gmail.com>
2020-03-25 16:06:22 +0100

    Final bug fixes

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,29 +1,72 @@
 import 'dart:wasm';
-
-import 'package:feetback/models/jump.dart';
-import 'package:firebase_database/firebase_database.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:feetback/utils/date_utils.dart';
 import 'package:intl/intl.dart';
 
+import 'package:feetback/models/jump.dart';
+
+import 'package:firebase_database/firebase_database.dart';
+
+import 'package:feetback/services/service_locator.dart';
+import 'package:feetback/services/auth_service.dart';
+
 class DatabaseService {
-  List<Jump> getAllJumps(String uid) async {
-    final DatabaseReference ourDB = FirebaseDatabase.instance.reference().child("users").child(uid).child("jumps");
-    return await ourDB.reference().orderByChild("date").once().value();
+  final AuthService _authService = locator<AuthService>();
+
+  Future<List<Jump>> getAllJumps() async {
+    if (await _authService.isUserSignedIn()) {
+      try {
+        final DatabaseReference ourDB = FirebaseDatabase.instance.reference().child("users").child(_authService.currentUser.uid).child("jumps");
+        DataSnapshot snap = await ourDB.reference().orderByChild("date").once();
+        Map<dynamic, dynamic> data = snap.value;
+        List<Jump> allJumps = new List<Jump>();
+        data.forEach((key, value) {
+        allJumps.add(Jump.fromDB(key, value));
+        });
+        allJumps.forEach((element) {
+          print (element.toString());
+        });
+        return allJumps;
+      } on Exception catch (e) {
+        print(e);
+      }
+    }
+
+    return null;
   }
 
-  Jump getHighestJump(String uid) async {
-    final DatabaseReference ourDB = FirebaseDatabase.instance.reference().child("users").child(uid).child("jumps");
-    return await ourDB.reference().orderByChild('height').limitToFirst(1);
+  Future<Jump> getHighestJump() async {
+    if (await _authService.isUserSignedIn()) {
+      try {
+        final DatabaseReference ourDB = FirebaseDatabase.instance.reference().child("users").child(_authService.currentUser.uid).child("jumps");
+        DataSnapshot snap = await ourDB.orderByChild("height").limitToLast(1).once();
+        Map<dynamic, dynamic> data = snap.value;
+        String highestJumpId = data.keys.first;
+        Jump highestJump = Jump.fromDB(highestJumpId, data[highestJumpId]);
+        print (highestJump.toString());
+        return highestJump;
+      } on Exception catch (e) {
+        print(e);
+      }
+    }
+    
+    return null;
   }  
-  void addJump(Float height, String uid) async {
-    final DatabaseReference ourDB = FirebaseDatabase.instance.reference().child("users").child(uid).child("jumps");
+
+  void addJump(double height, double airtime, bool isFavorite)async {
+    if (await _authService.isUserSignedIn()) {
+      final DatabaseReference ourDB = FirebaseDatabase.instance.reference().child("users").child(_authService.currentUser.uid).child("jumps");
     try {
       await ourDB.reference().push().set({
         'height': height,
-        'date': DateFormat('dd-MM-yyyy – hh:mm').format(DateTime.now())
+        'date': DateTime.now().toString(),
+        'airtime': airtime,
+        'favorite': isFavorite,
       });
+      print('Add successful');
     } on Exception catch (e) {
       print(e);
     }
+    }
+    
   }
 }
