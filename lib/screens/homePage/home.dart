0687ebb1387b9d64137f09b992faa644646714b0
ben <s108652@ap.be>
2020-03-30 17:59:56 +0200

    notifications working properly

--- lib/screens/homePage/home.dart
+++ lib/screens/homePage/home.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 import 'package:feetback/models/jump.dart';
@@ -12,67 +11,73 @@ import 'package:feetback/services/service_locator.dart';
 import 'package:feetback/widgets/feetback_app_bar.dart';
 
 class HomePage extends StatelessWidget {
-  final NotificationService _notificationService = locator<NotificationService>();
-  
-
+  final NotificationService _notificationService =
+      locator<NotificationService>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: FeetbackAppBar(
-        title: Text("Home"),
-        height: 92,
-        contentAlignment: Alignment.centerLeft,
-        padding: EdgeInsets.only(left: 16, right: 16),
-      ),
-      body: Container(
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            Text("Logged in as: "),
-            RaisedButton(
-              child: Text("Jump Details"),
-          
-              onPressed: () => Navigator.pushNamed(context, "/jumpdetails" , arguments: Jump(DateTime.now(), 177, 4)), //dummy data to test arguments passing
-              
-            ),
-            RaisedButton(
-              child: Text("Notifications"),
-              onPressed : () async {
-                await _notificationService.showWeeklyAtDayAndTime();
-              },
-            ),
-            RaisedButton(
-              child: Text("Notifications"),
-              onPressed : () async {
-                await  _notificationService.showNotification();
-              }
-            ),
-            RaisedButton(
-              child: Text("SignIn"),
-              onPressed: () => Navigator.pushNamed(context, "/signin"),
-            ),
-            RaisedButton(
-              child: Text("Jump History"),
-              onPressed: () => Navigator.pushNamed(context, "/jumphistory" ), 
-            ),
-            Text("Perform a jump only works if already connected. These buttons are for testing purpose and may be removed."),
-            RaisedButton(
-              child: Text("Connect to a mat"),
-              onPressed: () => Navigator.pushNamed(context, "/connect"),
-            ),
-            RaisedButton(
-              child: Text("Perform a jump"),
-              onPressed: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => JumpPage())),
-            )
-          ],
+        appBar: FeetbackAppBar(
+          title: Text("Home"),
+          height: 92,
+          contentAlignment: Alignment.centerLeft,
+          padding: EdgeInsets.only(left: 16, right: 16),
         ),
-      )
-    );
+        body: Container(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Text("Logged in as: "),
+              RaisedButton(
+                child: Text("Jump Details"),
+
+                onPressed: () => Navigator.pushNamed(context, "/jumpdetails",
+                    arguments: Jump(DateTime.now(), 177,
+                        4)), //dummy data to test arguments passing
+              ),
+              RaisedButton(
+                child: Text("Notifications week"),
+                onPressed: () async {
+                  await _notificationService.showWeeklyAtDayAndTime();
+                },
+              ),
+              RaisedButton(
+                child: Text("Notifications"),
+                onPressed: () async {
+                  await _notificationService.showNotification();
+                },
+              ),
+              RaisedButton(
+                child: Text("Notifications day"),
+                onPressed: () async {
+                  await _notificationService.showdailyAtTime();
+                },
+              ),
+              RaisedButton(
+                child: Text("SignIn"),
+                onPressed: () => Navigator.pushNamed(context, "/signin"),
+              ),
+              RaisedButton(
+                child: Text("Jump History"),
+                onPressed: () => Navigator.pushNamed(context, "/jumphistory"),
+              ),
+              Text(
+                  "Perform a jump only works if already connected. These buttons are for testing purpose and may be removed."),
+              RaisedButton(
+                child: Text("Connect to a mat"),
+                onPressed: () => Navigator.pushNamed(context, "/connect"),
+              ),
+              RaisedButton(
+                child: Text("Perform a jump"),
+                onPressed: () => Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => JumpPage())),
+              )
+            ],
+          ),
+        )
+      );
   }
 }
--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,30 +1,34 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'dart:async';
 
+
 class NotificationService {
-  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-  static final initializeSettingsAndroid = AndroidInitializationSettings('app_icon');
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  static final initializeSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
+
   static final initializeSettingsIOS = IOSInitializationSettings();
+
   final initializationSettings = InitializationSettings(
     initializeSettingsAndroid,
     initializeSettingsIOS,
   );
 
   NotificationService() {
-    _flutterLocalNotificationsPlugin.initialize(
+    flutterLocalNotificationsPlugin.initialize(
       initializationSettings,
-      onSelectNotification: onSelectNotification,
+     // onSelectNotification: onSelectNotification, 
     );
   }
 
-  Future onSelectNotification(String payload) async {
+ /* Future onSelectNotification(String payload) async {  // isn't quit necessary but am gonna leave it here for some time
     if (payload != null) {
       print('notification payload: ' + payload);
     }
-    
-  }
+  } */
 
-  String _toTwoDigitString(int value) {
+   String _toTwoDigitString(int value) {
     return value.toString().padLeft(2, '0');
   }
 
@@ -37,28 +41,47 @@ class NotificationService {
     var iOSPlatformChannelSpecifics = IOSNotificationDetails();
     var platformChannelSpecifics = NotificationDetails(
         androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
-    await _flutterLocalNotificationsPlugin.showWeeklyAtDayAndTime(
+    await flutterLocalNotificationsPlugin.showWeeklyAtDayAndTime(
         0,
         'show weekly title',
-        'Weekly notification shown on Monday at approximately ${_toTwoDigitString(time.hour)}:${_toTwoDigitString(time.minute)}:${_toTwoDigitString(time.second)}',
+        'Weekly notification shown on Monday at approximately',
         Day.Wednesday,
+        time,
+        platformChannelSpecifics,
+        payload: 'item x');
+  }
+
+   Future<void> showdailyAtTime() async {
+    var time = Time(16, 01, 00);
+    debugPrint(_toTwoDigitString(time.hour) +  ':' + _toTwoDigitString(time.minute) + ':' + _toTwoDigitString(time.second));
+    
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'repeatDailyAtTime channel id',
+        'repeatDailyAtTime channel name',
+        'repeatDailyAtTime description');
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.showDailyAtTime(
+        0,
+        'show daily title',
+        'Daily notification shown at approximately ${_toTwoDigitString(time.hour)}:${_toTwoDigitString(time.minute)}:${_toTwoDigitString(time.second)}',
         time,
         platformChannelSpecifics);
   }
 
-  
-
   Future<void> showNotification() async {
     var androidPlatformChannelSpecifics = AndroidNotificationDetails(
         'Bitch you should be sporting', 'FeetBack', 'sports brah',
-        importance: Importance.Max,
-        priority: Priority.High,
-        ticker: 'ticker');
+        importance: Importance.Max, priority: Priority.High, ticker: 'ticker');
     var iOSPlatformChannelSpecifics = IOSNotificationDetails();
     var platformChannelSpecifics = NotificationDetails(
         androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
-    await _flutterLocalNotificationsPlugin.show(
-        0, 'tity o mcdonalds', 'de koe ? die spreekt', platformChannelSpecifics,
+    await flutterLocalNotificationsPlugin.show(
+        0, 
+        'tity o mcdonalds', 
+        'de koe ? die spreekt', 
+        platformChannelSpecifics,
         payload: 'item x');
   }
 }
