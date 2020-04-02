849c2e08852266d0fcd6d8b602b3493fb030d7b6
Thibaut <weekx.t@gmail.com>
2020-04-02 13:42:40 +0200

    Getters & Setters methods for the app settings

--- lib/services/settings_service.dart
+++ lib/services/settings_service.dart
@@ -21,4 +21,46 @@ class SettingsService {
     SharedPreferences prefs = await _getInstance();
     return prefs.getBool('privacy_policy_acceptance') ?? false;
   }
+  // ------------------------------------------------
+  // -----------------App Settings-------------------
+  void setUnit(bool _isMetric) async {
+    SharedPreferences prefs = await _getInstance();
+    await prefs.setBool('isMetric', _isMetric);
+  }
+
+  Future<bool> get isMetric async {
+    SharedPreferences prefs = await _getInstance();
+    return prefs.getBool('isMetric') ?? false;
+  }
+
+  void setLang(String _lang) async {
+    SharedPreferences prefs = await _getInstance();
+    await prefs.setString('language', _lang);
+  }
+
+  Future<String> get getLang async {
+    SharedPreferences prefs = await _getInstance();
+    return prefs.getString('language') ?? 'en';
+  }
+
+  void setDevice(String _device) async {
+    SharedPreferences prefs = await _getInstance();
+    await prefs.setString('device', _device);
+  }
+
+  Future<String> get getDevice async {
+    SharedPreferences prefs = await _getInstance();
+    return prefs.getString('device') ?? 'Not Connected';
+  }
+
+  void setTheme(bool _isLight) async {
+    SharedPreferences prefs = await _getInstance();
+    await prefs.setBool('theme', _isLight);
+  }
+
+  Future<bool> get isLightTheme async {
+    SharedPreferences prefs = await _getInstance();
+    return prefs.getBool('theme') ?? true;
+  }
 }
+  
