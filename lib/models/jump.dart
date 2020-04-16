085cf040b90443564ebb9fcd8a5353c2c6711df8
Olivier Verhaegen <olivierverhaegen1@hotmail.com>
2020-04-16 12:22:08 +0200

    Bugfix jump id

--- lib/models/jump.dart
+++ lib/models/jump.dart
@@ -2,21 +2,22 @@ import 'package:feetback/models/standardization.dart';
 import 'package:fl_chart/fl_chart.dart';
 
 class Jump{
-    DateTime date;
-    double height;
-    double airtime;
-    bool favorite;
-  
-    Jump(DateTime date, double height, double airtime){
-        this.date = date;
-        this.height = height;
-        this.airtime = airtime;
-        this.favorite = false;
-    }    
+  String jid;
+  DateTime date;
+  double height;
+  double airtime;
+  bool favorite;
 
-    FlSpot getSpot(Standardization x){
-      return FlSpot(x.getStandard(this.date.millisecondsSinceEpoch.toDouble()), this.height);
-    }
+  Jump(DateTime date, double height, double airtime){
+      this.date = date;
+      this.height = height;
+      this.airtime = airtime;
+      this.favorite = false;
+  }    
+
+  FlSpot getSpot(Standardization x){
+    return FlSpot(x.getStandard(this.date.millisecondsSinceEpoch.toDouble()), this.height);
+  }
 
   Jump.fromDB(this.jid, Map data) {
     date = DateTime.parse(data['date']);
